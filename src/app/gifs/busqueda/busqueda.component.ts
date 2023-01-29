b83ce21d2577d8353cc5dcc0317806058fc13361
davidfb93 <david.banol.3889@eam.edu.co>
2023-01-28 19:16:14 -0500

    Final commit primera version

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,9 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { GifsService } from '../services/gifs.service';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
 })
 export class BusquedaComponent {
+
+  @ViewChild('txtBuscar') txtBuscar!: ElementRef<HTMLInputElement>;
+  // El ! es para dar a conocer el Non-null assertion operator
+
+  constructor( private GifsService: GifsService ) {}
+
+  buscar() {
+    const valor = this.txtBuscar.nativeElement.value;
+
+    // con esta validación evitamos guardar vacios
+    if ( valor.trim().length === 0) {
+      return;
+    }
+  
+    this.GifsService.buscarGifs( valor );
+
+    this.txtBuscar.nativeElement.value = '';
+  }
 
 }
